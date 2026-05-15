822c89d92b06806787d2895f51f8f6e9702f17a0
Mathachai <Mathachai363@gmail.com>
2026-05-16 01:07:57 +0700

    update version name

--- opencore_legacy_patcher/wx_gui/gui_main_menu.py
+++ opencore_legacy_patcher/wx_gui/gui_main_menu.py
@@ -67,7 +67,7 @@ class MainFrame(wx.Frame):
         logo.Centre(wx.HORIZONTAL)
 
         # Title label
-        title_label = wx.StaticText(self, label=f"OpenCore Legacy Patcher for T2 Macs Insider Preview", pos=(-1, 128))
+        title_label = wx.StaticText(self, label=f"OCLP-T2 Patcher Beta FixBugs", pos=(-1, 128))
         title_label.SetFont(gui_support.font_factory(25, wx.FONTWEIGHT_BOLD))
         title_label.Centre(wx.HORIZONTAL)
 
