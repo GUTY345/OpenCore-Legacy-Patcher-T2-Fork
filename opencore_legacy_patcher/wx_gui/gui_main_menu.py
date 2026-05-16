0b73fb50b64463033a67f67d00ba259f35ad762f
Albert Müller <p8bpg9zrw7@privaterelay.appleid.com>
2026-05-16 23:33:00 +0300

    Refactor description for Install drivers and patches
    Removed redundant description for the Install drivers and patches option.

--- opencore_legacy_patcher/wx_gui/gui_main_menu.py
+++ opencore_legacy_patcher/wx_gui/gui_main_menu.py
@@ -96,7 +96,8 @@ class MainFrame(wx.Frame):
             },
             "Install drivers and patches": {
                 "function": self.on_post_install_root_patch,
-                "description": ["Installs hardware drivers and", "patches for your system after", "installing a new version of macOS." /n/n,"This is the Install Root Patch button."],
+                "description": ["Installs hardware drivers and", "patches for your system after", "installing a new version of macOS."],
+                "description": ["This is the equivalent of the Install Root Patch button."],
                 "icon": str(self.constants.icns_resource_path / "OC-Patch.icns"),
             },
             "Support": {
