49d3dce1d4b4dd16ed4c0fdd955016b2b24ad5cd
Albert Müller <p8bpg9zrw7@privaterelay.appleid.com>
2026-05-26 14:51:20 +0300

    Refactor AMFI detection logic and add configuration checks

--- opencore_legacy_patcher/detections/amfi_detect.py
+++ opencore_legacy_patcher/detections/amfi_detect.py
@@ -1,63 +1,156 @@
 """
-amfi_data.py: AppleMobileFileIntegrity Bitmask Data
-"""
-
-"""
-Within AppleMobileFileIntegrity.kext, Apple has a bitmask-based boot-arg (ex. amfi=128)
-Below information is from reversed values in 13.0 Beta 6's AppleMobileFileIntegrity.kext
-Currently only 'amfi=3' has been used by Apple publicly
-- 0x3 used in 11.0.1 dyld source:
-  - https://github.com/apple-oss-distributions/dyld/blob/5c9192436bb195e7a8fe61f22a229ee3d30d8222/testing/test-cases/kernel-hello-world.dtest/main.c#L2
+amfi_detect.py: Determine AppleMobileFileIntegrity's OS configuration
 """
 
 import enum
 
+from ..support import utilities
+from ..datasets import amfi_data
 
-class AppleMobileFileIntegrity(enum.IntEnum):
-    # Names set are solely for readability
-    # Internal names are unknown
-    AMFI_ALLOW_TASK_FOR_PID:      int = 0x1   # Allow Task for PID (alt. amfi_unrestrict_task_for_pid=0x1)
-    AMFI_ALLOW_INVALID_SIGNATURE: int = 0x2   # Reduce sig enforcement (alt. amfi_allow_any_signature=0x1)
-    AMFI_LV_ENFORCE_THIRD_PARTY:  int = 0x4   # Don't mark external binaries as platform binaries
-    AMFI_UNKNOWN_1:               int = 0x8
-    AMFI_UNKNOWN_2:               int = 0x10
-    AMFI_UNKNOWN_3:               int = 0x20
-    AMFI_UNKNOWN_4:               int = 0x40
-    AMFI_ALLOW_EVERYTHING:        int = 0x80  # Disable sig enforcement and Library Validation (alt. amfi_get_out_of_my_way=0x1)
 
-"""
-Internally within AMFI.kext, Apple references 0x2 and 0x80 as both 'Disable signature enforcement'
-However 0x80 is a higher privilege than 0x2, and breaks TCC support in OS (ex. Camera, Microphone, etc prompts)
+class AmfiConfigDetectLevel(enum.IntEnum):
+    """
+    Configuration levels used by AmfiConfigurationDetection
+    """
 
-Supported boot-args within AMFI.kext, last compared against 13.0 Beta 6
+    NO_CHECK:                   int = 0
+    LIBRARY_VALIDATION:         int = 1  # For Ventura, use LIBRARY_VALIDATION_AND_SIG
+    LIBRARY_VALIDATION_AND_SIG: int = 2
+    ALLOW_ALL:                  int = 3
 
-  Within _initializeAppleMobileFileIntegrity():
-    - amfi_unrestrict_task_for_pid=0x1
-    - amfi_dev_mode_policy=0x1
-    - amfi_allow_any_signature=0x1
-    - amfi_get_out_of_my_way=0x1
-    - amfi_unrestricted_local_signing=0x1
-    - pmap_cs_unrestricted_local_signing=0x1
-    - amfi_ready_to_roll=0x1
-    - cs_enforcement_disable=0x1
 
-  Within AMFIInitializeLocalSigningPublicKey():
-    - -restore
+class AmfiConfigurationDetection:
+    """
+    Detect AppleMobileFileIntegrity's OS configuration
 
-  Within macOSPolicyConfigurationInit():
-    - amfi_force_policy=0x1
-    - amfi_block_unsigned_code=0x1
-    - amfi_force_cs_kill=0x1
-    - amfi_hsp_disable=0x1
-    - amfi_hsp_logging=0x1
-    - amfi_allow_bni_as_platform=0x1
-    - amfi_allow_non_platform=0x1
-    - amfi_prevent_old_entitled_platform_binaries=0x1
-    - amfi_allow_only_tc=0x1
-    - amfi_allow_only_tc_override=0x1
+    Usage:
 
-  Within configurationSettingsInit()
-    - amfi_enforce_launch_constraints=0x1
-    - amfi_allow_3p_launch_constraints=0x1
-    - BATS_TESTPLAN_ID="Custom Team ID"
-"""
+    >>> import amfi_detect
+    >>> can_patch = amfi_detect.AmfiConfigurationDetection().check_config(amfi_detect.AmfiConfigDetectLevel.ALLOW_ALL)
+
+    """
+
+    def __init__(self) -> None:
+        self.AMFI_ALLOW_TASK_FOR_PID:      bool = False
+        self.AMFI_ALLOW_INVALID_SIGNATURE: bool = False
+        self.AMFI_LV_ENFORCE_THIRD_PARTY:  bool = False
+        self.AMFI_ALLOW_EVERYTHING:        bool = False
+        self.SKIP_LIBRARY_VALIDATION:      bool = False
+
+        self.boot_args: list = []
+        self.oclp_args: list = []
+
+        self._init_nvram_dicts()
+
+        self._parse_amfi_bitmask()
+        self._parse_amfi_boot_args()
+        self._parse_oclp_configuration()
+
+
+    def _init_nvram_dicts(self) -> None:
+        """
+        Initialize the boot-args and OCLP-Settings NVRAM dictionaries
+        """
+
+        boot_args = utilities.get_nvram("boot-args", decode=True)
+        oclp_args = utilities.get_nvram("OCLP-Settings", "4D1FDA02-38C7-4A6A-9CC6-4BCCA8B30102", decode=True)
+
+        if boot_args:
+            self.boot_args = boot_args.split(" ")
+
+        if oclp_args:
+            self.oclp_args = oclp_args.split(" ")
+
+
+    def _parse_amfi_bitmask(self) -> None:
+        """
+        Parse the AMFI bitmask from boot-args
+        See data/amfi_data.py for more information
+        """
+
+        amfi_value = 0
+        for arg in self.boot_args:
+            if not arg.startswith("amfi="):
+                continue
+            try:
+                amfi_value = arg.split("=")
+                if len(amfi_value) != 2:
+                    return
+                amfi_value = amfi_value[1]
+                if amfi_value.startswith("0x"):
+                    amfi_value = int(amfi_value, 16)
+                else:
+                    amfi_value = int(amfi_value)
+            except:
+                return
+            break
+
+        if amfi_value == 0:
+            return
+
+        self.AMFI_ALLOW_TASK_FOR_PID:      bool = amfi_value & amfi_data.AppleMobileFileIntegrity.AMFI_ALLOW_TASK_FOR_PID
+        self.AMFI_ALLOW_INVALID_SIGNATURE: bool = amfi_value & amfi_data.AppleMobileFileIntegrity.AMFI_ALLOW_INVALID_SIGNATURE
+        self.AMFI_LV_ENFORCE_THIRD_PARTY:  bool = amfi_value & amfi_data.AppleMobileFileIntegrity.AMFI_LV_ENFORCE_THIRD_PARTY
+
+        if amfi_value & amfi_data.AppleMobileFileIntegrity.AMFI_ALLOW_EVERYTHING:
+            self.AMFI_ALLOW_EVERYTHING        = True
+            self.SKIP_LIBRARY_VALIDATION      = True
+            self.AMFI_ALLOW_INVALID_SIGNATURE = True
+
+
+    def _parse_amfi_boot_args(self) -> None:
+        """
+        Parse the AMFI boot-args
+        """
+
+        for arg in self.boot_args:
+            if arg.startswith("amfi_unrestrict_task_for_pid"):
+                value = arg.split("=")
+                if len(value) == 2:
+                    if value[1] in ["0x1", "1"]:
+                        self.AMFI_ALLOW_TASK_FOR_PID = True
+            elif arg.startswith("amfi_allow_any_signature"):
+                value = arg.split("=")
+                if len(value) == 2:
+                    if value[1] in ["0x1", "1"]:
+                        self.AMFI_ALLOW_INVALID_SIGNATURE = True
+            elif arg.startswith("amfi_get_out_of_my_way"):
+                value = arg.split("=")
+                if len(value) == 2:
+                    if value[1] in ["0x1", "1"]:
+                        self.AMFI_ALLOW_EVERYTHING = True
+                        self.SKIP_LIBRARY_VALIDATION = True
+                        self.AMFI_ALLOW_INVALID_SIGNATURE = True
+
+
+    def _parse_oclp_configuration(self) -> None:
+        """
+        Parse the OCLP configuration
+        """
+
+        if "-allow_amfi" in self.oclp_args:
+            self.SKIP_LIBRARY_VALIDATION = True
+
+
+    def check_config(self, level: int) -> bool:
+        """
+        Check the AMFI configuration based on provided AMFI level
+        See AmfiConfigLevel enum for valid levels
+
+        Parameters:
+            level (int): The level of AMFI checks to check for
+
+        Returns:
+            bool: True if the AMFI configuration matches the level, False otherwise
+        """
+
+        if level == AmfiConfigDetectLevel.NO_CHECK:
+            return True
+        if level == AmfiConfigDetectLevel.LIBRARY_VALIDATION:
+            return self.SKIP_LIBRARY_VALIDATION
+        if level == AmfiConfigDetectLevel.LIBRARY_VALIDATION_AND_SIG:
+            return bool(self.SKIP_LIBRARY_VALIDATION and self.AMFI_ALLOW_INVALID_SIGNATURE)
+        if level == AmfiConfigDetectLevel.ALLOW_ALL:
+            return self.AMFI_ALLOW_EVERYTHING
+
+        return False
