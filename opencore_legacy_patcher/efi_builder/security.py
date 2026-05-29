3f50ef3b07e7b932c32977ec86af652691043154
Albert Müller <p8bpg9zrw7@privaterelay.appleid.com>
2026-05-30 02:08:41 +0300

    Update security.py

--- opencore_legacy_patcher/efi_builder/security.py
+++ opencore_legacy_patcher/efi_builder/security.py
@@ -310,7 +310,7 @@ class BuildSecurity:
             kernel_patches.append({
                 "Arch": "x86_64",
                 "Comment": "Increase T2 USB Timeout (UI Stall fix)",
-                "Enabled": False,
+                "Enabled": True,
                 "Identifier": "com.apple.driver.usb.AppleUSBXHCI",
                 "Find": binascii.unhexlify("BA0A000000"),
                 "Replace": binascii.unhexlify("BAFF000000"),
